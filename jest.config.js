743d607e2b8bef403345ac109227262b94e57f9d
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-11-24 18:59:47 +1300

    Add tests for the scraper. Run on GHA

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+    testEnvironment: 'jsdom',
+    globals: {
+        fetch: global.fetch,
+    },
+};
+
+module.exports = config;
--- nerdlets/nr1-browser-agent-version-pinning/scrape.js
+++ nerdlets/nr1-browser-agent-version-pinning/scrape.js
@@ -1,33 +1,30 @@
 export const scrapeBrowserAgentData = async () => {
     const URL = 'https://docs.newrelic.com/docs/browser/browser-monitoring/getting-started/browser-agent-eol-policy/';
 
-    try {
-        const response = await fetch(URL);
-        const html = await response.text();
-
-        const parser = new DOMParser();
-        const document = parser.parseFromString(html, 'text/html');
-
-        const table = document.querySelector('table');
-
-        if (!table) {
-            console.error('No table found on the page.');
-            return [];
-        }
-
-        const rows = table.querySelectorAll('tbody tr');
-
-        return Array.from(rows).map((row) => {
-            const cells = row.querySelectorAll('td');
-
-            return {
-                version: cells[0]?.textContent.trim(),
-                startDate: new Date(cells[1]?.textContent.trim()),
-                endDate: new Date(cells[2]?.textContent.trim()),
-            };
-        });
-    } catch (error) {
-        console.error('Error scraping data:', error);
-        return [];
+    const response = await fetch(URL);
+    if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
     }
+    const html = await response.text();
+
+    const parser = new DOMParser();
+    const document = parser.parseFromString(html, 'text/html');
+
+    const table = document.querySelector('table');
+
+    if (!table) {
+        throw new Error('No table found on the page.');
+    }
+
+    const rows = table.querySelectorAll('tbody tr');
+
+    return Array.from(rows).map((row) => {
+        const cells = row.querySelectorAll('td');
+
+        return {
+            version: cells[0]?.textContent.trim(),
+            startDate: new Date(cells[1]?.textContent.trim()),
+            endDate: new Date(cells[2]?.textContent.trim()),
+        };
+    });
 };
