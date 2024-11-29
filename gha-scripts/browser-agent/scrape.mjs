a85adc1614c7f0aefb260c084d443af5e4f6a058
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-11-29 20:31:01 +1300

    Push to gh-pages branch

--- gha-scripts/browser-agent/scrape.mjs
+++ gha-scripts/browser-agent/scrape.mjs
@@ -23,7 +23,7 @@ async function scrapeBrowserAgentData() {
         });
     });
 
-    if (data.length === 0 || data[0].version || !data[0].startDate || !data[0].endDate) {
+    if (data.length === 0 || !data[0].version || !data[0].startDate || !data[0].endDate) {
         const space = 2;
         throw new Error(`Invalid data detected. Data:\n${JSON.stringify(data, null, space)}`);
     }
