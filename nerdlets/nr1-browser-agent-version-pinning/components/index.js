d93fec95805abcb374d49172d581428682c02626
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-07 16:18:05 +1300

    Folders. Introduce usePinnedVersion hook

---
+++ nerdlets/nr1-browser-agent-version-pinning/components/index.js
@@ -0,0 +1,5 @@
+import BrowserAgentTable from './BrowserAgentTable';
+import ConfirmationModal from './ConfirmationModal';
+import CustomVersionForm from './CustomVersionForm';
+
+export { BrowserAgentTable, ConfirmationModal, CustomVersionForm };
--- nerdlets/nr1-browser-agent-version-pinning/index.js
+++ nerdlets/nr1-browser-agent-version-pinning/index.js
@@ -1,70 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import {
-    NerdGraphQuery,
-    NerdGraphMutation,
-    ngql,
-    InlineMessage,
+    BlockText,
+    Button,
     Card,
-    CardHeader,
     CardBody,
+    CardHeader,
     CardSection,
     Grid,
     GridItem,
-    NerdletStateContext,
-    Toast,
     HeadingText,
-    BlockText,
+    InlineMessage,
     Link,
+    NerdGraphMutation,
+    NerdletStateContext,
     Tabs,
     TabsItem,
-    Button,
+    Toast,
 } from 'nr1';
-import BrowserAgentTable from './BrowserAgentTable';
-import CustomVersionForm from './CustomVersionForm';
-import ConfirmationModal from './ConfirmationModal';
+import BrowserAgentTable from './components/BrowserAgentTable/BrowserAgentTable';
+import CustomVersionForm from './components/CustomVersionForm/CustomVersionForm';
+import ConfirmationModal from './components/ConfirmationModal/ConfirmationModal';
+import { UPDATE_PINNED_VERSION } from './graphql/mutations';
+import { usePinnedVersion } from './hooks/usePinnedVersion';
 
-// TODO: Consider the name of the component
 function Nr1BrowserAgentVersionPinningNerdlet() {
-    const [currentVersion, setCurrentVersion] = useState('');
     const [newVersion, setNewVersion] = useState(null);
-    const [browserAppGuid, setBrowserAppGuid] = useState('');
     const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
-    useEffect(() => {
-        if (browserAppGuid) {
-            fetchPinnedVersion(browserAppGuid);
-        }
-    }, [browserAppGuid]);
-
-    const isVersionEmpty = !currentVersion;
-
-    const fetchPinnedVersion = async (browserAppGuid) => {
-        const query = `
-            query FetchBrowserMonitoringAgentSettings {
-                actor {
-                    entity(guid: "${browserAppGuid}") {
-                        ... on BrowserApplicationEntity {
-                            browserSettings {
-                                browserMonitoring {
-                                    pinnedVersion
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        `;
-
-        try {
-            const response = await NerdGraphQuery.query({ query });
-            const pinnedVersion = response?.data?.actor?.entity?.browserSettings?.browserMonitoring?.pinnedVersion;
-
-            setCurrentVersion(pinnedVersion);
-        } catch (error) {
-            console.error('Error fetching pinned version:', error);
-            setCurrentVersion('Error fetching version');
-        }
-    };
+    const { entityGuid } = useContext(NerdletStateContext);
+    // TODO: Add a loading state using pinnedVersionLoading. Show error message using pinnedVersionError.
+    const { pinnedVersion, setPinnedVersion, pinnedVersionError, pinnedVersionLoading } = usePinnedVersion(entityGuid);
 
     const handleUpdateVersion = async (pinnedVersion) => {
         setNewVersion(pinnedVersion);
@@ -72,39 +37,17 @@ function Nr1BrowserAgentVersionPinningNerdlet() {
     };
 
     const onUpdateVersion = async () => {
-        if (!browserAppGuid) {
-            console.error('Browser App GUID is missing.');
-            return;
-        }
-
         try {
             const response = await NerdGraphMutation.mutate({
-                mutation: ngql`
-                    mutation($guid: EntityGuid!, $pinnedVersion: String) {
-                        agentApplicationSettingsUpdate(
-                            guid: $guid
-                            settings: { browserMonitoring: { pinnedVersion: $pinnedVersion } }
-                        ) {
-                            browserProperties {
-                                jsLoaderScript
-                            }
-                            browserSettings {
-                                browserMonitoring {
-                                    loader
-                                    pinnedVersion
-                                }
-                            }
-                        }
-                    }
-                `,
+                mutation: UPDATE_PINNED_VERSION,
                 variables: {
-                    guid: browserAppGuid,
+                    guid: entityGuid,
                     pinnedVersion: newVersion,
                 },
             });
 
             if (response.errors) {
-                console.error('Error updating pinned version:', response.errors);
+                console.error('Error updating pinned version', response.errors);
                 Toast.showToast({
                     title: 'Update Failed',
                     description: 'Failed to update pinned version',
@@ -121,10 +64,10 @@ function Nr1BrowserAgentVersionPinningNerdlet() {
             });
 
             setShowConfirmationModal(false);
-            setCurrentVersion(newVersion);
+            setPinnedVersion(newVersion);
         } catch (error) {
-            console.error('Mutation failed:', error);
             setShowConfirmationModal(false);
+            console.error('Error updating pinned version', error);
             Toast.showToast({
                 title: 'Update Failed',
                 description: 'Failed to update pinned version',
@@ -135,137 +78,104 @@ function Nr1BrowserAgentVersionPinningNerdlet() {
     };
 
     return (
-        <NerdletStateContext.Consumer>
-            {(nerdletState) => {
-                const browserAppGuid = nerdletState?.entityGuid;
-                setBrowserAppGuid(browserAppGuid);
-                if (!browserAppGuid) {
-                    return (
-                        <InlineMessage
-                            type={InlineMessage.TYPE.CRITICAL}
-                            label="No entity context found."
-                            description="This Nerdlet must be opened within an entity view."
-                        />
-                    );
-                }
-
-                return (
-                    <>
-                        <ConfirmationModal
-                            showModal={showConfirmationModal}
-                            currentVersion={currentVersion}
-                            newVersion={newVersion}
-                            onConfirm={onUpdateVersion}
-                            onCancel={() => setShowConfirmationModal(false)}
-                        />
-                        <Grid>
-                            <GridItem columnSpan={12}>
-                                <Card>
-                                    <CardHeader>
-                                        <HeadingText type={HeadingText.TYPE.HEADING_4}>Version pinning</HeadingText>
-                                    </CardHeader>
-                                    <CardBody>
-                                        <BlockText
-                                            spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}
-                                        >
-                                            Pin a specific version of the New Relic Browser agent to ensure platform
-                                            consistency.
-                                        </BlockText>
-                                        <BlockText
-                                            spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}
-                                        >
-                                            This extension for New Relic One simplifies browser agent version pinning,
-                                            eliminating the need to use the NerdGraph API directly.
-                                        </BlockText>
-                                        <BlockText
-                                            spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}
-                                        >
-                                            For more information, see the {/*TODO: Consts file for links*/}
-                                            <Link to="https://docs.newrelic.com/docs/apis/nerdgraph/examples/browser-monitoring-config-nerdgraph/#browser-agent-version-pinning">
-                                                Browser Agent Version Pinning documentation
-                                            </Link>
-                                        </BlockText>
-                                    </CardBody>
-                                </Card>
-                            </GridItem>
-                            <GridItem columnSpan={12}>
-                                <Card>
-                                    <CardHeader>
-                                        <HeadingText type={HeadingText.TYPE.HEADING_4}>
-                                            Current pinning status
-                                        </HeadingText>
-                                    </CardHeader>
-                                    <CardBody>
-                                        <CardSection>
-                                            {isVersionEmpty ? (
-                                                <InlineMessage
-                                                    type={InlineMessage.TYPE.INFO}
-                                                    label={`No version pinned`}
-                                                />
-                                            ) : (
-                                                <InlineMessage
-                                                    type={InlineMessage.TYPE.SUCCESS}
-                                                    label={`Pinned version: ${currentVersion}`}
-                                                />
-                                            )}
-                                        </CardSection>
-                                        <CardSection>
-                                            <Button disabled={isVersionEmpty} onClick={() => handleUpdateVersion(null)}>
-                                                Remove Pinning
-                                            </Button>
-                                        </CardSection>
-                                    </CardBody>
-                                </Card>
-                            </GridItem>
-                            <GridItem columnSpan={12}>
-                                <Card>
-                                    <CardHeader>
-                                        <HeadingText
-                                            type={HeadingText.TYPE.HEADING_4}
-                                            spacingType={[
-                                                HeadingText.SPACING_TYPE.MEDIUM,
-                                                HeadingText.SPACING_TYPE.NONE,
-                                            ]}
-                                        >
-                                            Manage pinning
-                                        </HeadingText>
-                                    </CardHeader>
-                                    <CardBody>
-                                        <Tabs defaultValue="supported">
-                                            <TabsItem value="supported" label="Supported versions">
-                                                <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
-                                                    The versions in the table below are the{' '}
-                                                    <Link to="https://docs.newrelic.com/docs/browser/browser-monitoring/getting-started/browser-agent-eol-policy/">
-                                                        currently supported versions of the New Relic browser agent
-                                                    </Link>
-                                                </BlockText>
-                                                {/*TODO: What happens when I do not have permission to update pinning?*/}
-                                                <BrowserAgentTable
-                                                    currentPinnedVersion={currentVersion}
-                                                    onUpdateVersion={handleUpdateVersion}
-                                                />
-                                            </TabsItem>
-                                            <TabsItem value="custom" label="Custom version">
-                                                <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
-                                                    Specify a version based on the release number from{' '}
-                                                    <Link to="https://github.com/newrelic/newrelic-browser-agent/releases">
-                                                        GitHub
-                                                    </Link>
-                                                </BlockText>
-                                                <CustomVersionForm
-                                                    currentPinnedVersion={currentVersion}
-                                                    onUpdateVersion={handleUpdateVersion}
-                                                />
-                                            </TabsItem>
-                                        </Tabs>
-                                    </CardBody>
-                                </Card>
-                            </GridItem>
-                        </Grid>
-                    </>
-                );
-            }}
-        </NerdletStateContext.Consumer>
+        <>
+            <ConfirmationModal
+                showModal={showConfirmationModal}
+                currentVersion={pinnedVersion}
+                newVersion={newVersion}
+                onConfirm={onUpdateVersion}
+                onCancel={() => setShowConfirmationModal(false)}
+            />
+            <Grid>
+                <GridItem columnSpan={12}>
+                    <Card>
+                        <CardHeader>
+                            <HeadingText type={HeadingText.TYPE.HEADING_4}>Version pinning</HeadingText>
+                        </CardHeader>
+                        <CardBody>
+                            <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}>
+                                Pin a specific version of the New Relic Browser agent to ensure platform consistency.
+                            </BlockText>
+                            <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}>
+                                This extension for New Relic One simplifies browser agent version pinning, eliminating
+                                the need to use the NerdGraph API directly.
+                            </BlockText>
+                            <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM, BlockText.SPACING_TYPE.NONE]}>
+                                For more information, see the {/*TODO: Consts file for links*/}
+                                <Link to="https://docs.newrelic.com/docs/apis/nerdgraph/examples/browser-monitoring-config-nerdgraph/#browser-agent-version-pinning">
+                                    Browser Agent Version Pinning documentation
+                                </Link>
+                            </BlockText>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+                <GridItem columnSpan={12}>
+                    <Card>
+                        <CardHeader>
+                            <HeadingText type={HeadingText.TYPE.HEADING_4}>Current pinning status</HeadingText>
+                        </CardHeader>
+                        <CardBody>
+                            <CardSection>
+                                {pinnedVersion ? (
+                                    <InlineMessage
+                                        type={InlineMessage.TYPE.SUCCESS}
+                                        label={`Pinned version: ${pinnedVersion}`}
+                                    />
+                                ) : (
+                                    <InlineMessage type={InlineMessage.TYPE.INFO} label={`No version pinned`} />
+                                )}
+                            </CardSection>
+                            <CardSection>
+                                <Button disabled={!pinnedVersion} onClick={() => handleUpdateVersion(null)}>
+                                    Remove Pinning
+                                </Button>
+                            </CardSection>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+                <GridItem columnSpan={12}>
+                    <Card>
+                        <CardHeader>
+                            <HeadingText
+                                type={HeadingText.TYPE.HEADING_4}
+                                spacingType={[HeadingText.SPACING_TYPE.MEDIUM, HeadingText.SPACING_TYPE.NONE]}
+                            >
+                                Manage pinning
+                            </HeadingText>
+                        </CardHeader>
+                        <CardBody>
+                            <Tabs defaultValue="supported">
+                                <TabsItem value="supported" label="Supported versions">
+                                    <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
+                                        The versions in the table below are the{' '}
+                                        <Link to="https://docs.newrelic.com/docs/browser/browser-monitoring/getting-started/browser-agent-eol-policy/">
+                                            currently supported versions of the New Relic browser agent
+                                        </Link>
+                                    </BlockText>
+                                    {/*TODO: What happens when I do not have permission to update pinning?*/}
+                                    <BrowserAgentTable
+                                        currentPinnedVersion={pinnedVersion}
+                                        onUpdateVersion={handleUpdateVersion}
+                                    />
+                                </TabsItem>
+                                <TabsItem value="custom" label="Custom version">
+                                    <BlockText spacingType={[BlockText.SPACING_TYPE.MEDIUM]}>
+                                        Specify a version based on the release number from{' '}
+                                        <Link to="https://github.com/newrelic/newrelic-browser-agent/releases">
+                                            GitHub
+                                        </Link>
+                                    </BlockText>
+                                    <CustomVersionForm
+                                        currentPinnedVersion={pinnedVersion}
+                                        onUpdateVersion={handleUpdateVersion}
+                                    />
+                                </TabsItem>
+                            </Tabs>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+            </Grid>
+        </>
     );
 }
 
