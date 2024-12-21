33e614c5f7a5d20376f1548196579238acdb70bf
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-21 23:41:30 +1300

    TODO about the hook for modal ...

--- nerdlets/nr1-browser-agent-version-pinning/hooks/useModal.js
+++ nerdlets/nr1-browser-agent-version-pinning/hooks/useModal.js
@@ -2,5 +2,6 @@ import { useContext } from "react";
 import { ModalContext } from "../context";
 
 export function useModal() {
+  // TODO: Consider only returning the "openModal" (and possibly "closeModal") functions
   return useContext(ModalContext);
 }
