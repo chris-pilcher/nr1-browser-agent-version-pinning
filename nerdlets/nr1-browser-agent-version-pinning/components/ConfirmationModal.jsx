3508582cbfe2ed7d957e203ebec993b4ec22060b
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-25 22:18:54 +1300

    Add enum for different update types

--- nerdlets/nr1-browser-agent-version-pinning/components/ConfirmationModal.jsx
+++ nerdlets/nr1-browser-agent-version-pinning/components/ConfirmationModal.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { useContext } from "react";
 import { BlockText, Button, HeadingText, Modal, Stack, StackItem, Toast } from "nr1";
 import { usePinnedVersionMutation, usePinnedVersionQuery } from "../hooks";
 import { ModalContext } from "../context";
@@ -8,41 +8,43 @@ export default function ConfirmationModal() {
   const pinnedVersionQuery = usePinnedVersionQuery();
   const pinnedVersionMutation = usePinnedVersionMutation();
 
-  const isRemovingPinning = newVersion === null;
-  const titleText = isRemovingPinning ? "Remove Pinning" : "Update Pinning";
-  const actionText = isRemovingPinning ? "Remove" : "Pin";
-  const messageText = createMessageText(isRemovingPinning, pinnedVersionQuery.data, newVersion);
+  const updateType = parseUpdateType(pinnedVersionQuery.data, newVersion);
+
+  const title = {
+    [UpdateType.REMOVE_PINNING]: "Remove Pinning",
+    [UpdateType.ADD_PINNING]: "Add Pinning",
+    [UpdateType.UPDATE_PINNING]: "Update Pinning",
+  };
+
+  const actionText = {
+    [UpdateType.REMOVE_PINNING]: "Remove",
+    [UpdateType.ADD_PINNING]: "Pin",
+    [UpdateType.UPDATE_PINNING]: "Update",
+  };
+
+  const description = {
+    [UpdateType.REMOVE_PINNING]: `Are you sure you want to remove the ${pinnedVersionQuery.data} version pinning?`,
+    [UpdateType.ADD_PINNING]: `Are you sure you want to pin to version ${newVersion}?`,
+    [UpdateType.UPDATE_PINNING]: `Are you sure you want to update the version pinning from ${pinnedVersionQuery.data} to ${newVersion}?`,
+  };
+
+  const handleUpdatePinnedVersion = () => {
+    pinnedVersionMutation.mutate(newVersion, {
+      onSuccess: () => showSuccessToast(newVersion),
+      onError: () => showErrorToast(handleUpdatePinnedVersion),
+      onSettled: closeModal,
+    });
+  };
 
   const closeModal = () => {
     setHidden(true);
   };
 
-  const handleUpdatePinnedVersion = () => {
-    pinnedVersionMutation.mutate(newVersion, {
-      onSuccess: () => {
-        Toast.showToast({
-          title: "Updated",
-          description: newVersion ? `Pinned version updated to ${newVersion}` : "Pinning removed",
-          type: Toast.TYPE.NORMAL,
-        });
-      },
-      onError: () => {
-        Toast.showToast({
-          title: "Update Failed",
-          description: "Failed to update pinned version",
-          actions: [{ label: "Retry", onClick: handleUpdatePinnedVersion }],
-          type: Toast.TYPE.CRITICAL,
-        });
-      },
-      onSettled: closeModal,
-    });
-  };
-
   return (
     <Modal hidden={hidden} onClose={closeModal}>
-      <HeadingText type={HeadingText.TYPE.HEADING_3}>{titleText}</HeadingText>
+      <HeadingText type={HeadingText.TYPE.HEADING_3}>{title[updateType]}</HeadingText>
       <BlockText spacingType={[BlockText.SPACING_TYPE.EXTRA_LARGE, BlockText.SPACING_TYPE.OMIT]}>
-        {messageText}
+        {description[updateType]}
       </BlockText>
       <Stack>
         <StackItem>
@@ -50,10 +52,10 @@ export default function ConfirmationModal() {
         </StackItem>
         <StackItem>
           <Button
-            type={isRemovingPinning ? Button.TYPE.DESTRUCTIVE : Button.TYPE.PRIMARY}
+            type={updateType === UpdateType.REMOVE_PINNING ? Button.TYPE.DESTRUCTIVE : Button.TYPE.PRIMARY}
             onClick={handleUpdatePinnedVersion}
             loading={pinnedVersionMutation.isLoading}>
-            {actionText}
+            {actionText[updateType]}
           </Button>
         </StackItem>
       </Stack>
@@ -61,27 +63,35 @@ export default function ConfirmationModal() {
   );
 }
 
-// TODO: This is a bit long. Maybe make it more concise? maybe a map?
-function createMessageText(isRemovingPinning, currentVersion, newVersion) {
-  if (isRemovingPinning) {
-    return (
-      <Fragment>
-        Are you sure you want to remove the <b>{currentVersion}</b> version pinning?
-      </Fragment>
-    );
-  }
+const UpdateType = {
+  REMOVE_PINNING: "remove",
+  ADD_PINNING: "pin",
+  UPDATE_PINNING: "change",
+};
 
-  if (currentVersion === null) {
-    return (
-      <Fragment>
-        Are you sure you want to pin to version <b>{newVersion}</b>?
-      </Fragment>
-    );
+function parseUpdateType(currentVersion, newVersion) {
+  if (!newVersion) {
+    return UpdateType.REMOVE_PINNING;
+  } else if (!currentVersion) {
+    return UpdateType.ADD_PINNING;
+  } else {
+    return UpdateType.UPDATE_PINNING;
   }
+}
 
-  return (
-    <Fragment>
-      Are you sure you want to update the version pinning from {currentVersion} to <b>{newVersion}</b>?
-    </Fragment>
-  );
+function showSuccessToast(version) {
+  Toast.showToast({
+    title: "Updated",
+    description: version ? `Pinned version updated to ${version}` : "Pinning removed",
+    type: Toast.TYPE.NORMAL,
+  });
+}
+
+function showErrorToast(retry) {
+  Toast.showToast({
+    title: "Update Failed",
+    description: "Failed to update pinned version",
+    actions: [{ label: "Retry", onClick: retry }],
+    type: Toast.TYPE.CRITICAL,
+  });
 }
