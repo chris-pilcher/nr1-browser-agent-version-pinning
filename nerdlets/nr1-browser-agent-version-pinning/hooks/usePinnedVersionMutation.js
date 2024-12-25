4e5cad72728746183c61fb99bc3ca4bc3936a492
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-26 10:09:45 +1300

    Move the queries into the hooks

--- nerdlets/nr1-browser-agent-version-pinning/hooks/usePinnedVersionMutation.js
+++ nerdlets/nr1-browser-agent-version-pinning/hooks/usePinnedVersionMutation.js
@@ -1,17 +1,30 @@
 import { useContext } from "react";
-import { NerdGraphMutation, NerdletStateContext } from "nr1";
-import { UPDATE_PINNED_VERSION } from "../graphql/mutations";
+import { NerdGraphMutation, NerdletStateContext, ngql } from "nr1";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { PINNED_VERSION_QUERY_KEY } from "./queryKeys";
 
 export default function usePinnedVersionMutation() {
   const { entityGuid } = useContext(NerdletStateContext);
   const queryClient = useQueryClient();
+  const mutation = ngql`
+    mutation($guid: EntityGuid!, $pinnedVersion: String) {
+      agentApplicationSettingsUpdate(
+        guid: $guid
+        settings: { browserMonitoring: { pinnedVersion: $pinnedVersion } }
+      ) {
+        browserSettings {
+          browserMonitoring {
+            pinnedVersion
+          }
+        }
+      }
+    }
+  `;
 
   return useMutation({
     mutationFn: (pinnedVersion) => {
       return NerdGraphMutation.mutate({
-        mutation: UPDATE_PINNED_VERSION,
+        mutation,
         variables: { guid: entityGuid, pinnedVersion },
       });
     },
--- nerdlets/nr1-browser-agent-version-pinning/hooks/usePinnedVersionQuery.js
+++ nerdlets/nr1-browser-agent-version-pinning/hooks/usePinnedVersionQuery.js
@@ -1,17 +1,31 @@
 import { useContext } from "react";
-import { NerdGraphQuery, NerdletStateContext } from "nr1";
-import { FETCH_PINNED_VERSION } from "../graphql/queries";
+import { NerdGraphQuery, NerdletStateContext, ngql } from "nr1";
 import { useQuery } from "@tanstack/react-query";
 import { PINNED_VERSION_QUERY_KEY } from "./queryKeys";
 
 export default function usePinnedVersionQuery() {
   const { entityGuid } = useContext(NerdletStateContext);
+  const query = ngql`
+    query FetchBrowserMonitoringAgentSettings($browserAppGuid: EntityGuid!) {
+      actor {
+        entity(guid: $browserAppGuid) {
+          ... on BrowserApplicationEntity {
+            browserSettings {
+              browserMonitoring {
+                pinnedVersion
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
 
   return useQuery({
     queryKey: PINNED_VERSION_QUERY_KEY,
     queryFn: () =>
       NerdGraphQuery.query({
-        query: FETCH_PINNED_VERSION,
+        query,
         variables: { browserAppGuid: entityGuid },
       }).then(({ data, errors }) => {
         if (errors) {
