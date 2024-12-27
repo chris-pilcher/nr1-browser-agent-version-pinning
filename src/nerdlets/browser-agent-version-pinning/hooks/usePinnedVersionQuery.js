bc891c58194c7650b9f3634465e2f5398b7c800a
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-28 12:06:24 +1300

    UI looks good when it's unable to load the current version

--- src/nerdlets/browser-agent-version-pinning/hooks/usePinnedVersionQuery.js
+++ src/nerdlets/browser-agent-version-pinning/hooks/usePinnedVersionQuery.js
@@ -29,7 +29,6 @@ export default function usePinnedVersionQuery() {
         variables: { browserAppGuid: entityGuid },
       }).then(({ data, errors }) => {
         if (errors) {
-          // TODO: Check this actually works and how it looks in the UI when each condition is triggered. Use a debugger to check as well
           throw new Error(errors);
         }
         return data.actor.entity.browserSettings.browserMonitoring.pinnedVersion;
