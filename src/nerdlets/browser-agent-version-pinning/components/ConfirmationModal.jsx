9bc76bce9c95d0586a732702c035d073ac7869bd
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2024-12-29 18:53:24 +1300

    Show loading status when opening modal

--- src/nerdlets/browser-agent-version-pinning/components/ConfirmationModal.jsx
+++ src/nerdlets/browser-agent-version-pinning/components/ConfirmationModal.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { BlockText, Button, HeadingText, Modal, Stack, StackItem, Toast } from "nr1";
+import React, { useContext, Fragment } from "react";
+import { BlockText, Button, HeadingText, Modal, SectionMessage, Spinner, Stack, StackItem, Toast } from "nr1";
 import { usePinnedVersionMutation, usePinnedVersionQuery } from "../hooks";
 import { ModalContext } from "../context";
 
@@ -8,7 +8,8 @@ export default function ConfirmationModal() {
   const pinnedVersionQuery = usePinnedVersionQuery();
   const pinnedVersionMutation = usePinnedVersionMutation();
 
-  const updateType = parseUpdateType(pinnedVersionQuery.data, newVersion);
+  const version = pinnedVersionQuery.data;
+  const updateType = parseUpdateType(version, newVersion);
 
   const title = {
     [UpdateType.ADD_PINNING]: "Add Pinning",
@@ -24,8 +25,8 @@ export default function ConfirmationModal() {
 
   const description = {
     [UpdateType.ADD_PINNING]: `Are you sure you want to pin to version ${newVersion}?`,
-    [UpdateType.UPDATE_PINNING]: `Are you sure you want to update the version pinning from ${pinnedVersionQuery.data} to ${newVersion}?`,
-    [UpdateType.REMOVE_PINNING]: `Are you sure you want to remove the ${pinnedVersionQuery.data} version pinning?`,
+    [UpdateType.UPDATE_PINNING]: `Are you sure you want to update the version pinning from ${version} to ${newVersion}?`,
+    [UpdateType.REMOVE_PINNING]: `Are you sure you want to remove the ${version} version pinning?`,
   };
 
   const handleUpdatePinnedVersion = () => {
@@ -42,23 +43,35 @@ export default function ConfirmationModal() {
 
   return (
     <Modal hidden={hidden} onClose={closeModal}>
-      <HeadingText type={HeadingText.TYPE.HEADING_3}>{title[updateType]}</HeadingText>
-      <BlockText spacingType={[BlockText.SPACING_TYPE.EXTRA_LARGE, BlockText.SPACING_TYPE.OMIT]}>
-        {description[updateType]}
-      </BlockText>
-      <Stack>
-        <StackItem>
-          <Button onClick={closeModal}>Cancel</Button>
-        </StackItem>
-        <StackItem>
-          <Button
-            type={updateType === UpdateType.REMOVE_PINNING ? Button.TYPE.DESTRUCTIVE : Button.TYPE.PRIMARY}
-            onClick={handleUpdatePinnedVersion}
-            loading={pinnedVersionMutation.isLoading}>
-            {actionText[updateType]}
-          </Button>
-        </StackItem>
-      </Stack>
+      {pinnedVersionQuery.isLoading && <Spinner inline />}
+      {pinnedVersionQuery.isError && (
+        <SectionMessage
+          type={SectionMessage.TYPE.CRITICAL}
+          title="Failed to fetch pinned version"
+          description="An error occurred while trying to fetch the current pinned version. Please try again later."
+        />
+      )}
+      {pinnedVersionQuery.isSuccess && (
+        <Fragment>
+          <HeadingText type={HeadingText.TYPE.HEADING_3}>{title[updateType]}</HeadingText>
+          <BlockText spacingType={[BlockText.SPACING_TYPE.EXTRA_LARGE, BlockText.SPACING_TYPE.OMIT]}>
+            {description[updateType]}
+          </BlockText>
+          <Stack>
+            <StackItem>
+              <Button onClick={closeModal}>Cancel</Button>
+            </StackItem>
+            <StackItem>
+              <Button
+                type={updateType === UpdateType.REMOVE_PINNING ? Button.TYPE.DESTRUCTIVE : Button.TYPE.PRIMARY}
+                onClick={handleUpdatePinnedVersion}
+                loading={pinnedVersionMutation.isLoading}>
+                {actionText[updateType]}
+              </Button>
+            </StackItem>
+          </Stack>
+        </Fragment>
+      )}
     </Modal>
   );
 }
